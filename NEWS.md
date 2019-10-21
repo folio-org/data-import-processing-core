10800e3c1aa4eddf3d03e5ac6a11840c36f807ba
Igor-Gorchakov <Igor_Gorchakov@epam.com>
2019-10-21 23:19:31 +0300

    Initial module setup

---
+++ NEWS.md
@@ -0,0 +1,2 @@
+## 1.0.0 2019-10-21
+ * Initial module setup
---
+++ src/main/java/org/folio/processing/Stub.java
@@ -0,0 +1,7 @@
+package org.folio.processing;
+
+/**
+ * STUB source class to build artifacts. Remove it after sources add.
+ */
+public class Stub {
+}
