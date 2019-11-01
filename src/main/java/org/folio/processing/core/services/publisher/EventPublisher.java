a0d90c50bb0190348f814c01b974ec6dc3dcd2f5
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2019-11-01 16:09:34 +0200

    Updated javadoc

--- src/main/java/org/folio/processing/core/services/publisher/EventPublisher.java
+++ src/main/java/org/folio/processing/core/services/publisher/EventPublisher.java
@@ -8,7 +8,7 @@ import org.folio.processing.core.model.EventContext;
  */
 public interface EventPublisher {
   /**
-   * Send an event to consumer service, that is mod-pubsub or other ones.
+   * Send event to consumer service, which may be mod-pubsub or other ones.
    *
    * @param context even context
    * @return future
