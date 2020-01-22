963bd4dabb808e41815f2b4faf625537089700d9
Kateryna Senchenko <kateryna_senchenko@epam.com>
2020-01-22 12:16:54 +0200

    Refactoring

--- src/main/java/org/folio/processing/matching/reader/util/MatchExpressionUtil.java
+++ src/main/java/org/folio/processing/matching/reader/util/MatchExpressionUtil.java
@@ -5,9 +5,6 @@ import org.folio.processing.matching.model.schemas.Qualifier;
 
 import static org.folio.processing.matching.model.schemas.Qualifier.ComparisonPart.ALPHANUMERICS_ONLY;
 import static org.folio.processing.matching.model.schemas.Qualifier.ComparisonPart.NUMERICS_ONLY;
-import static org.folio.processing.matching.model.schemas.Qualifier.QualifierType.BEGINS_WITH;
-import static org.folio.processing.matching.model.schemas.Qualifier.QualifierType.CONTAINS;
-import static org.folio.processing.matching.model.schemas.Qualifier.QualifierType.ENDS_WITH;
 
 /**
  * Util class to process value according to MatchExpression
@@ -17,20 +14,19 @@ public final class MatchExpressionUtil {
   private static final String NON_DIGIT = "[^\\p{N}]";
   private static final String NON_DIGIT_AND_NON_ALPHA = "[^\\p{L}\\p{N}]";
 
-  private MatchExpressionUtil() {}
+  private MatchExpressionUtil() {
+  }
 
   /**
    * Extracts specified comparison part of the value
    *
-   * @param value original value
+   * @param value     original value
    * @param qualifier qualifier specifying which comparison part should be extracted from the value
    * @return comparison part of the value
    */
   public static String extractComparisonPart(String value, Qualifier qualifier) {
-    if (value != null) {
-      if (qualifier == null || qualifier.getComparisonPart() == null) {
-        return value;
-      } else if (qualifier.getComparisonPart() == NUMERICS_ONLY) {
+    if (value != null && qualifier != null && qualifier.getComparisonPart() != null) {
+      if (qualifier.getComparisonPart() == NUMERICS_ONLY) {
         return value.replaceAll(NON_DIGIT, StringUtils.EMPTY);
       } else if (qualifier.getComparisonPart() == ALPHANUMERICS_ONLY) {
         return value.replaceAll(NON_DIGIT_AND_NON_ALPHA, StringUtils.EMPTY);
@@ -42,23 +38,24 @@ public final class MatchExpressionUtil {
   /**
    * Checks whether value is qualified to be compared in matching process
    *
-   * @param value original value
+   * @param value     original value
    * @param qualifier qualifier specifying conditions that value should satisfy in order to be used for matching purposes
    * @return true if value is qualified for matching
    */
   public static boolean isQualified(String value, Qualifier qualifier) {
-    if (value != null) {
-      if (qualifier == null || qualifier.getQualifierType() == null) {
-        return true;
-      } else if (qualifier.getQualifierType() == BEGINS_WITH) {
-        return value.startsWith(qualifier.getQualifierValue());
-      } else if (qualifier.getQualifierType() == ENDS_WITH) {
-        return value.endsWith(qualifier.getQualifierValue());
-      } else if (qualifier.getQualifierType() == CONTAINS) {
-        return value.contains(qualifier.getQualifierValue());
+    boolean isQualified = true;
+    if (value != null && qualifier != null && qualifier.getQualifierType() != null) {
+      switch (qualifier.getQualifierType()) {
+        case BEGINS_WITH: isQualified = value.startsWith(qualifier.getQualifierValue());
+        break;
+        case ENDS_WITH: isQualified = value.endsWith(qualifier.getQualifierValue());
+        break;
+        case CONTAINS: isQualified = value.contains(qualifier.getQualifierValue());
+        break;
+        default: isQualified = false;
       }
     }
-     return false;
+    return isQualified;
   }
 
 }
