fc209682f4ea89dd8e09a763aa21219d6beea96c
Kateryna Senchenko <kateryna_senchenko@epam.com>
2020-03-06 18:27:50 +0200

    Added exception constructors

--- src/main/java/org/folio/processing/exceptions/EventProcessingException.java
+++ src/main/java/org/folio/processing/exceptions/EventProcessingException.java
@@ -2,7 +2,15 @@ package org.folio.processing.exceptions;
 
 public class EventProcessingException extends RuntimeException {
 
+  public EventProcessingException(Throwable cause) {
+    super(cause);
+  }
+
   public EventProcessingException(String message) {
     super(message);
+  }
+
+  public EventProcessingException(String message, Throwable cause) {
+    super(message, cause);
   }
 }
--- src/main/java/org/folio/processing/exceptions/MappingException.java
+++ src/main/java/org/folio/processing/exceptions/MappingException.java
@@ -5,4 +5,12 @@ public class MappingException extends RuntimeException {
   public MappingException(Throwable cause) {
     super(cause);
   }
+
+  public MappingException(String message) {
+    super(message);
+  }
+
+  public MappingException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
