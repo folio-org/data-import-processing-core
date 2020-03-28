64ece0de068ed57da9c697413260013175ab0a88
Oleksii Kuzminov <40833111+oleksiikuzminov@users.noreply.github.com>
2020-03-28 14:58:09 +0200

    fix class cast (#26)
    (cherry picked from commit 63f56d3)

--- src/main/java/org/folio/processing/matching/matcher/Matcher.java
+++ src/main/java/org/folio/processing/matching/matcher/Matcher.java
@@ -1,21 +1,29 @@
 package org.folio.processing.matching.matcher;
 
+import io.vertx.core.json.JsonObject;
 import org.folio.DataImportEventPayload;
 import org.folio.MatchDetail;
 import org.folio.MatchProfile;
-import org.folio.rest.jaxrs.model.ProfileSnapshotWrapper;
 import org.folio.processing.matching.loader.LoadResult;
 import org.folio.processing.matching.loader.MatchValueLoader;
 import org.folio.processing.matching.loader.query.LoadQuery;
 import org.folio.processing.matching.loader.query.LoadQueryBuilder;
 import org.folio.processing.matching.reader.MatchValueReader;
 import org.folio.processing.value.Value;
+import org.folio.rest.jaxrs.model.ProfileSnapshotWrapper;
+
+import java.util.Map;
 
 public interface Matcher {
 
   default boolean match(MatchValueReader matchValueReader, MatchValueLoader matchValueLoader, DataImportEventPayload eventPayload) {
     ProfileSnapshotWrapper matchingProfileWrapper = eventPayload.getCurrentNode();
-    MatchProfile matchProfile = (MatchProfile) matchingProfileWrapper.getContent();
+    MatchProfile matchProfile;
+    if (matchingProfileWrapper.getContent() instanceof Map) {
+      matchProfile = new JsonObject((Map) matchingProfileWrapper.getContent()).mapTo(MatchProfile.class);
+    } else {
+      matchProfile = (MatchProfile) matchingProfileWrapper.getContent();
+    }
     // Only one matching detail is expected in first implementation,
     // in future matching will support multiple matching details combined in logic expressions
     MatchDetail matchDetail = matchProfile.getMatchDetails().get(0);
