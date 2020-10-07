11cc0bdefea4d47ddf260947b080f2c87fb4a662
Kateryna Senchenko <42532596+KaterynaSenchenko@users.noreply.github.com>
2020-10-07 17:37:49 +0300

    MODDICORE-85 Added support for matching by multiple values (#104)
    * MODDICORE-85 Added support for matching by multiple values
    
    * Switch to match by ANY

--- NEWS.md
+++ NEWS.md
@@ -6,6 +6,7 @@
 * [MODSOURMAN-281](https://issues.folio.org/browse/MODSOURMAN-281) Added support for event post-processing
 * [MODDICORE-77](https://issues.folio.org/browse/MODDICORE-77) Applied MARC field mapping protection settings
 * [MODSOURCE-184](https://issues.folio.org/browse/MODSOURCE-184) Added support for "Update" option of mapping profile for marc bib modification
+* [MODDICORE-85](https://issues.folio.org/browse/MODDICORE-85) Added support to match by multiple values (fix matching MARC 035 to Instance Identifier)
 * [MODINV-346](https://issues.folio.org/browse/MODINV-346) Problem with the repeatable check in/out notes field mapping actions[BUGFIX]
 
 ## 2020-08-10 v2.1.6
--- src/main/java/org/folio/processing/matching/reader/MarcValueReaderImpl.java
+++ src/main/java/org/folio/processing/matching/reader/MarcValueReaderImpl.java
@@ -1,31 +1,13 @@
 package org.folio.processing.matching.reader;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.folio.DataImportEventPayload;
 import org.folio.MatchDetail;
-import org.folio.Record;
-import org.folio.processing.exceptions.ReaderException;
 import org.folio.processing.matching.reader.util.MarcValueReaderUtil;
-import org.folio.processing.value.ListValue;
 import org.folio.processing.value.MissingValue;
-import org.folio.processing.value.StringValue;
 import org.folio.processing.value.Value;
 import org.folio.rest.jaxrs.model.EntityType;
-import org.folio.rest.jaxrs.model.Field;
 import org.folio.rest.jaxrs.model.MatchExpression;
-import org.apache.commons.lang3.StringUtils;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static org.folio.processing.matching.reader.util.MatchExpressionUtil.extractComparisonPart;
-import static org.folio.processing.matching.reader.util.MatchExpressionUtil.isQualified;
 import static org.folio.rest.jaxrs.model.EntityType.MARC_AUTHORITY;
 import static org.folio.rest.jaxrs.model.EntityType.MARC_BIBLIOGRAPHIC;
 import static org.folio.rest.jaxrs.model.EntityType.MARC_HOLDINGS;
