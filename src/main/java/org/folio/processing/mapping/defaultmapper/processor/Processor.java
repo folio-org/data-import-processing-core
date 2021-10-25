343e5e1acbd27adf7eaad5a94f320897c21a8e58
AivarIUsupov <aivar_iusupov@epam.com>
2021-10-25 17:37:08 +0300

    MODDICORE-187 Added debug logging for processing skip

--- src/main/java/org/folio/processing/mapping/defaultmapper/processor/Processor.java
+++ src/main/java/org/folio/processing/mapping/defaultmapper/processor/Processor.java
@@ -316,6 +316,8 @@ public class Processor<T> {
       expandSubfields(subFields, splitter);
     }
     if (subFields.stream().noneMatch(sf -> (checkIfSubfieldShouldBeHandled(subFieldsSet, sf)))) {
+      //skip further processing if there are no subfields to map
+      LOGGER.info("no subfields to map from {} to {}", subFields.stream().map(Subfield::getCode).collect(Collectors.toList()), subFieldsSet);
       return;
     }
 
