967aefe7e04fef74f6598fd892d149b296f641a9
Khamidulla Abdulkhakimov <Khamidulla_Abdulkhakimov@epam.com>
2021-12-15 17:56:12 +0500

    MODDATAIMP-592 Added checking multiple cases for the test

--- src/test/java/org/folio/processing/matching/loader/LoadQueryBuilderTest.java
+++ src/test/java/org/folio/processing/matching/loader/LoadQueryBuilderTest.java
@@ -15,6 +15,7 @@ import org.folio.rest.jaxrs.model.Qualifier;
 import org.folio.rest.jaxrs.model.EntityType;
 import org.folio.rest.jaxrs.model.MatchExpression;
 import org.folio.rest.jaxrs.model.StaticValueDetails;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -34,10 +35,7 @@ import static org.folio.MatchDetail.MatchCriterion.INCOMING_VALUE_CONTAINS_EXIST
 import static org.folio.MatchDetail.MatchCriterion.INCOMING_VALUE_ENDS_WITH_EXISTING_VALUE;
 import static org.folio.rest.jaxrs.model.MatchExpression.DataValueType.STATIC_VALUE;
 import static org.folio.rest.jaxrs.model.MatchExpression.DataValueType.VALUE_FROM_RECORD;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
 public class LoadQueryBuilderTest {
@@ -669,15 +667,33 @@ public class LoadQueryBuilderTest {
           new Field().withLabel("field").withValue("instance.identifiers[].value"),
           new Field().withLabel("identifierTypeId").withValue(identifierTypeFieldValue))
         ));
+
+    MatchDetail matchDetailWithWrongSecondField = new MatchDetail()
+      .withMatchCriterion(EXACTLY_MATCHES)
+      .withIncomingRecordType(EntityType.MARC_BIBLIOGRAPHIC)
+      .withExistingRecordType(EntityType.INSTANCE)
+      .withExistingMatchExpression(new MatchExpression()
+        .withDataValueType(VALUE_FROM_RECORD)
+        .withFields(Arrays.asList(
+          new Field().withLabel("field").withValue("instance.identifiers[].value"),
+          new Field().withLabel("identifierTypeIdr").withValue(identifierTypeFieldValue))
+        ));
+
     //when
     LoadQuery result = LoadQueryBuilder.build(value, matchDetail);
+    LoadQuery wrongResult = LoadQueryBuilder.build(value, matchDetailWithWrongSecondField);
     //then
     assertNotNull(result);
+    assertNotNull(wrongResult);
     assertNotNull(result.getSql());
+    assertNotNull(wrongResult.getSql());
     String expectedSQLQuery = StringUtils.EMPTY;
     assertEquals(expectedSQLQuery, result.getSql());
+    assertNotEquals(expectedSQLQuery, wrongResult.getSql());
     assertNotNull(result.getCql());
+    assertNotNull(wrongResult.getCql());
     String expectedCQLQuery = format("(identifiers= /@value/@identifierTypeId=%s (%s))",identifierTypeFieldValue, value.getValue());
     assertEquals(expectedCQLQuery, result.getCql());
+    assertNotEquals(expectedCQLQuery, wrongResult.getCql());
   }
 }
