0ee1058d6332da3061a684bc1a84ef7c7cc3a31e
Roman Chernetskyi <Roman_Chernetskyi@epam.com>
2023-03-14 19:26:44 +0200

    [MODDICORE-304] Allow matching string values from incoming accepted values

--- src/main/java/org/folio/processing/mapping/MappingManager.java
+++ src/main/java/org/folio/processing/mapping/MappingManager.java
@@ -141,8 +141,14 @@ public final class MappingManager {
           if (Objects.equals(mappingRule.getName(), STATISTICAL_CODE_IDS)) {
             for (RepeatableSubfieldMapping subfield : mappingRule.getSubfields()) {
               for (MappingRule field : subfield.getFields()) {
-                if(Objects.equals(field.getName(), STATISTICAL_CODE_ID)) {
-                  field.setAcceptedValues(statisticalCodes);
+                if (Objects.equals(field.getName(), STATISTICAL_CODE_ID)) {
+                  if (field.getAcceptedValues() != null) {
+                    if (!field.getAcceptedValues().containsValue(field.getValue().replace("\"", ""))) {
+                      field.getAcceptedValues().putAll(statisticalCodes);
+                    }
+                  } else {
+                    field.withAcceptedValues(statisticalCodes);
+                  }
                 }
               }
             }
