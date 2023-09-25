038ae13c5564e467fc433cd7f518370954f4c1e6
yaroslav-epam <138673581+yaroslav-epam@users.noreply.github.com>
2023-09-25 16:24:41 +0300

    MODDICORE-360: migrate to folio-kafka-wrapper 3.0.0 version (#308)

--- src/main/java/org/folio/processing/events/services/publisher/KafkaEventPublisher.java
+++ src/main/java/org/folio/processing/events/services/publisher/KafkaEventPublisher.java
@@ -5,13 +5,14 @@ import io.vertx.core.json.Json;
 import io.vertx.kafka.client.producer.KafkaHeader;
 import io.vertx.kafka.client.producer.KafkaProducer;
 import io.vertx.kafka.client.producer.KafkaProducerRecord;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.DataImportEventPayload;
 import org.folio.kafka.KafkaConfig;
 import org.folio.kafka.KafkaTopicNameHelper;
+import org.folio.kafka.SimpleKafkaProducerManager;
+import org.folio.kafka.services.KafkaProducerRecordBuilder;
 import org.folio.processing.events.utils.PomReaderUtil;
 import org.folio.rest.jaxrs.model.Event;
 import org.folio.rest.jaxrs.model.EventMetadata;
@@ -30,7 +31,6 @@ public class KafkaEventPublisher implements EventPublisher {
   private static final Logger LOGGER = LogManager.getLogger(KafkaEventPublisher.class);
   public static final String RECORD_ID_HEADER = "recordId";
   public static final String CHUNK_ID_HEADER = "chunkId";
-
 
   private static final AtomicLong indexer = new AtomicLong();
 
@@ -67,42 +67,47 @@ public class KafkaEventPublisher implements EventPublisher {
           .withEventTTL(1)
           .withPublishedBy(PomReaderUtil.INSTANCE.constructModuleVersionAndVersion(PomReaderUtil.INSTANCE.getModuleName(), PomReaderUtil.INSTANCE.getVersion())));
 
-      String key = String.valueOf(indexer.incrementAndGet() % maxDistributionNum);
-
       String topicName = KafkaTopicNameHelper.formatTopicName(kafkaConfig.getEnvId(), KafkaTopicNameHelper.getDefaultNameSpace(),
         eventPayload.getTenant(), eventType);
 
-      KafkaProducerRecord<String, String> record =
-        KafkaProducerRecord.create(topicName, key, Json.encode(event));
+      var record = buildRecord(eventPayload, event, topicName);
+      record.addHeaders(getHeaders(eventPayload, recordId, chunkId, jobExecutionId));
 
-      List<KafkaHeader> headers = new ArrayList<>();
-      headers.add(KafkaHeader.header(OKAPI_URL_HEADER, eventPayload.getOkapiUrl()));
-      headers.add(KafkaHeader.header(OKAPI_TENANT_HEADER, eventPayload.getTenant()));
-      headers.add(KafkaHeader.header(OKAPI_TOKEN_HEADER, eventPayload.getToken()));
-      checkAndAddHeaders(recordId, chunkId, jobExecutionId, headers);
-
-      record.addHeaders(headers);
-
-      String producerName = eventType + "_Producer";
-      KafkaProducer<String, String> producer =
-        KafkaProducer.createShared(vertx, producerName, kafkaConfig.getProducerProps());
-
-      producer.write(record, war -> {
-        producer.end(ear -> producer.close());
-        if (war.succeeded()) {
-          LOGGER.info("publish:: Event with type: '{}' by jobExecutionId: '{}' and recordId: '{}' with chunkId: '{}' was sent to the topic '{}' ", eventType, jobExecutionId, recordId, chunkId, topicName);
+      KafkaProducer<String, String> producer = new SimpleKafkaProducerManager(vertx, kafkaConfig).createShared(eventType);
+      producer.send(record)
+        .onSuccess(ar -> {
+          LOGGER.info("publish:: Event with type: '{}' by jobExecutionId: '{}' and recordId: '{}' with chunkId: '{}' was sent to the topic '{}' ",
+            eventType, jobExecutionId, recordId, chunkId, topicName);
           future.complete(event);
-        } else {
-          Throwable cause = war.cause();
-          LOGGER.warn("publish:: {} write error for event: '{}' by jobExecutionId: '{}' with recordId: '{}' and with chunkId: '{}' ", producerName, jobExecutionId, eventType, recordId, chunkId, cause);
-          future.completeExceptionally(cause);
-        }
-      });
+        })
+        .onFailure(error -> {
+          LOGGER.warn("publish:: {} send error for event: '{}' by jobExecutionId: '{}' with recordId: '{}' and with chunkId: '{}' ",
+            eventType + "_Producer", eventType, jobExecutionId, recordId, chunkId, error);
+          future.completeExceptionally(error);
+        });
     } catch (Exception e) {
       LOGGER.warn("publish:: Can not publish event: {} with recordId: {}", eventType, recordId, e);
       future.completeExceptionally(e);
     }
     return future;
+  }
+
+  private KafkaProducerRecord<String, String> buildRecord(DataImportEventPayload eventPayload, Event event, String topicName) {
+    String key = String.valueOf(indexer.incrementAndGet() % maxDistributionNum);
+    return new KafkaProducerRecordBuilder<String, Object>(eventPayload.getTenant())
+      .key(key)
+      .value(event)
+      .topic(topicName)
+      .build();
+  }
+
+  private List<KafkaHeader> getHeaders(DataImportEventPayload eventPayload, String recordId, String chunkId, String jobExecutionId) {
+    List<KafkaHeader> headers = new ArrayList<>();
+    headers.add(KafkaHeader.header(OKAPI_URL_HEADER, eventPayload.getOkapiUrl()));
+    headers.add(KafkaHeader.header(OKAPI_TENANT_HEADER, eventPayload.getTenant()));
+    headers.add(KafkaHeader.header(OKAPI_TOKEN_HEADER, eventPayload.getToken()));
+    checkAndAddHeaders(recordId, chunkId, jobExecutionId, headers);
+    return headers;
   }
 
   private void checkAndAddHeaders(String recordId, String chunkId, String jobExecutionId, List<KafkaHeader> headers) {
