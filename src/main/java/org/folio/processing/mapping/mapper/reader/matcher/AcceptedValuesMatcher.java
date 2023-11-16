bbb5a2257372dde4667abc7726f8bc9fa5eb3c44
Ruslan Lavrov <ruslan_lavrov@epam.com>
2023-11-16 13:47:53 +0200

    Added doc comments

--- src/main/java/org/folio/processing/mapping/mapper/reader/matcher/AcceptedValuesMatcher.java
+++ src/main/java/org/folio/processing/mapping/mapper/reader/matcher/AcceptedValuesMatcher.java
@@ -1,7 +1,20 @@
 package org.folio.processing.mapping.mapper.reader.matcher;
 
+/**
+ * A comparison function, which determines whether the specified value
+ * matches to accepted value retrieved from a {@link org.folio.MappingProfile}
+ */
+@FunctionalInterface
 public interface AcceptedValuesMatcher {
 
+  /**
+   * Checks whether specified {@code valueToCompare} matches to the {@code acceptedValue}
+   * by criteria that depend on this method implementation.
+   *
+   * @param acceptedValue   - accepted value from the mapping profile
+   * @param valueToCompare  - value to compare to the accepted value
+   * @return true if the {@code valueToCompare} matches to the {@code acceptedValue}, otherwise false
+   */
   boolean matches(String acceptedValue, String valueToCompare);
 
 }
