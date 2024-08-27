8c86c40bc75c6e8ff53a71139c06de06c4e388ae
Javokhir Abdullaev <javokhir_abdullaev@epam.com>
2024-08-27 12:10:26 +0500

    test premature closure of producers

--- src/main/java/org/folio/processing/events/EventManager.java
+++ src/main/java/org/folio/processing/events/EventManager.java
@@ -159,7 +159,10 @@ public final class EventManager {
         .flatMap(mappingParent -> findParent(mappingParent.getId(), jobProfileSnapshot)
           .flatMap(actionParent -> getNextChildProfile(mappingParent, actionParent)
             .or(() -> findParent(actionParent.getId(), jobProfileSnapshot)
-              .flatMap(matchParent -> getNextChildProfile(actionParent, matchParent))
+              .flatMap(matchParent -> getNextChildProfile(actionParent, matchParent)
+                .or(() -> findParent(matchParent.getId(), jobProfileSnapshot)
+                  .flatMap(parentMatchParent -> getNextChildProfile(matchParent, parentMatchParent)))
+              )
             )));
     }
     if (currentNode.getContentType() == ACTION_PROFILE) {
@@ -219,15 +222,15 @@ public final class EventManager {
    * @param vertx       - vertx instance
    */
   public static void registerKafkaEventPublisher(KafkaConfig kafkaConfig, Vertx vertx, int maxDistributionNum) {
-    eventPublisher.forEach(p -> {
-      if(p instanceof KafkaEventPublisher publisher) {
-        try {
-          publisher.close();
-        } catch (Exception e) {
-          LOGGER.error(e.getMessage(), e);
-        }
-      }
-    });
+//    eventPublisher.forEach(p -> {
+//      if(p instanceof KafkaEventPublisher publisher) {
+//        try {
+//          publisher.close();
+//        } catch (Exception e) {
+//          LOGGER.error(e.getMessage(), e);
+//        }
+//      }
+//    });
     eventPublisher.clear();
     eventPublisher.add(new KafkaEventPublisher(kafkaConfig, vertx, maxDistributionNum));
   }
