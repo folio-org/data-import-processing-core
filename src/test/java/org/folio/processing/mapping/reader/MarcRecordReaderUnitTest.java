34aa674dd777e092a5ae98f6fa86db9b65821d5f
RomanChernetskyi <111740564+RomanChernetskyi@users.noreply.github.com>
2024-09-19 12:12:37 +0300

    [MODORDERS-1181] Add orders mapping parameters (#358)

--- src/test/java/org/folio/processing/mapping/reader/MarcRecordReaderUnitTest.java
+++ src/test/java/org/folio/processing/mapping/reader/MarcRecordReaderUnitTest.java
@@ -836,7 +836,7 @@ public class MarcRecordReaderUnitTest {
     MappingContext mappingContext = new MappingContext();
     mappingContext.setMappingParameters(new MappingParameters()
       .withInitializedState(true)
-      .withTenantConfiguration("{\"locale\":\"en-US\",\"timezone\":\"Pacific/Kiritimati\",\"currency\":\"USD\"}"));
+      .withTenantConfigurationZone("{\"locale\":\"en-US\",\"timezone\":\"Pacific/Kiritimati\",\"currency\":\"USD\"}"));
 
     eventPayload.setContext(context);
     Reader reader = new MarcBibReaderFactory().createReader();
@@ -867,7 +867,7 @@ public class MarcRecordReaderUnitTest {
     eventPayload.setContext(context);
 
     MappingContext mappingContext = new MappingContext().withMappingParameters(new MappingParameters()
-      .withTenantConfiguration("{\"locale\":\"en-US\",\"timezone\":\"asdas/sadas\",\"currency\":\"USD\"}")
+      .withTenantConfigurationZone("{\"locale\":\"en-US\",\"timezone\":\"asdas/sadas\",\"currency\":\"USD\"}")
       .withInitializedState(true));
 
     Reader reader = new MarcBibReaderFactory().createReader();
