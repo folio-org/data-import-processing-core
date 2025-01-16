172eafd078125a2edf994ad7971154128cddcc64
elena-shmygaliova <eshmygaliova@ebsco.com>
2025-01-16 11:28:29 +0300

    MODDICORE-426-436 authorities custom mapping Ramsons (#384)
    * MODDICORE-426: Custom mapping for saft* fields
    
    * MODDICORE-436: Apply "--" subFieldDelimiter to AuthorityExtended fields
    
    ---------
    
    Co-authored-by: ElenaShm <elena.shmygaliova@gmail.com>

--- src/main/java/org/folio/processing/mapping/defaultmapper/processor/Processor.java
+++ src/main/java/org/folio/processing/mapping/defaultmapper/processor/Processor.java
@@ -63,8 +63,9 @@ public class Processor<T> {
   private static final Map<Class<?>, Map<String, Method>> METHOD_CACHE = new ConcurrentHashMap<>();
   private static final Map<Field, ParameterizedType> PARAM_TYPE_CACHE = new ConcurrentHashMap<>();
   public static final String ALTERNATIVE_MAPPING = "alternativeMapping";
-  private static final List<String> DEFAULT_SUBFIELDS = List.of("a", "b", "c", "d", "e", "f", "g", "h", "i",
-    "j", "k", "l", "m", "n", "o", "p", "r", "q", "s", "t", "v", "x", "y", "z", "4", "5");
+  private static final String FIELDS_WITH_TRUNCATED_MAPPING_POSTFIX = "Trunc";
+  private static final String SAFT_FIELDS_PREFIX = "saft";
+  public static final String DELIMITER_SUBFIELDS = "subfields";
 
   private JsonObject mappingRules;
 
@@ -166,6 +167,7 @@ public class Processor<T> {
   private JsonArray getDataFieldMapping(DataField dataField) {
     JsonArray mappingArray = mappingRules.getJsonArray(dataField.getTag());
     if (entity instanceof AuthorityExtended) {
+      addSubFieldDelimiterForAuthorities(dataField, mappingArray);
       return addExtraMappingsForAuthorities(dataField, mappingArray);
     }
     return mappingArray;
@@ -477,7 +479,7 @@ public class Processor<T> {
       for (int i = 0; i < delimiters.size(); i++) {
         JsonObject job = delimiters.getJsonObject(i);
         String delimiter = job.getString(VALUE);
-        JsonArray subFieldswithDel = job.getJsonArray("subfields");
+        JsonArray subFieldswithDel = job.getJsonArray(DELIMITER_SUBFIELDS);
         StringBuilder subFieldsStringBuilder = new StringBuilder();
         buffers2concat.add(subFieldsStringBuilder);
         if (subFieldswithDel.size() == 0) {
@@ -950,28 +952,112 @@ public class Processor<T> {
   }
 
   /**
+   * Extends regular entity mapping for 1xx, 4xx, 5xx field with "subFieldDelimiter"
+   * by adding the following structure to the mapping:
+   * "subFieldDelimiter": [
+   *             {
+   *               "value": " ",
+   *               "subfields": [
+   *                 "a","b","c","d","t","f","g",...
+   *               ]
+   *             },
+   *             {
+   *               "value": "--",
+   *               "subfields": [
+   *                 "x","y","z","v"
+   *               ]
+   *             },
+   *             {
+   *               "value": "--",
+   *               "subfields": []
+   *             }
+   *           ]
+   */
+  private void addSubFieldDelimiterForAuthorities(DataField dataField, JsonArray mappingArray) {
+    if (!dataField.getTag().startsWith("1")
+      && !dataField.getTag().startsWith("4")
+      && !dataField.getTag().startsWith("5")) {
+      return;
+    }
+    final List<String> doubleDashedSubfields = List.of("x", "y", "z", "v");
+    List<LinkedHashMap<String, Object>> mappingList = mappingArray.getList();
+    mappingList.forEach(mapping -> {
+      List<String> subfields = (List) mapping.get(SUBFIELD);
+      if (subfields == null || subfields.stream().noneMatch(doubleDashedSubfields::contains)) {
+        return;
+      }
+      List<LinkedHashMap<String, Object>> subFieldDelimiterList = new ArrayList<>();
+      LinkedHashMap<String, Object> spaceDelimiter = new LinkedHashMap<>(Map.of(VALUE, " ", DELIMITER_SUBFIELDS,
+        subfields.stream().filter(s -> !doubleDashedSubfields.contains(s)).toList()));
+      subFieldDelimiterList.add(spaceDelimiter);
+      subFieldDelimiterList.add(new LinkedHashMap<>(Map.of(VALUE, "--", DELIMITER_SUBFIELDS,
+        doubleDashedSubfields)));
+      subFieldDelimiterList.add(new LinkedHashMap<>(Map.of(VALUE, "--", DELIMITER_SUBFIELDS, List.of())));
+      mapping.put("subFieldDelimiter", subFieldDelimiterList);
+      }
+    );
+  }
+
+  /**
    * Extends regular entity mapping for 5xx field with one according to the following rules:
    * additionally to the regular mapping adds the mapping for targets:
+   *
    * saftBroaderTerm,  when the control subfield $w has "g" value
    * saftNarrowerTerm, when the control subfield $w has "h" value
    * saftEarlierHeading, when the control subfield $w has "a" value
    * saftLaterHeading, when the control subfield $w has "b" value.
+   *
+   * saft*Trunc for every saft* field with "i" and numeric subfields excluded
    */
   private JsonArray addExtraMappingsForAuthorities(final DataField dataField, final JsonArray regularMapping) {
     boolean is5XXField = dataField.getTag().startsWith("5");
-    if (!is5XXField || dataField.getSubfield('w') == null || regularMapping == null || regularMapping.isEmpty()) {
+    if (!is5XXField || regularMapping == null || regularMapping.isEmpty()) {
       return regularMapping;
     }
     final JsonArray extendedMapping = new JsonArray();
     List<String> targets = retrieveTargetsFromControlSubfield(dataField);
     List<LinkedHashMap<String, Object>> mappingList = regularMapping.getList();
-    targets.forEach(target -> extendedMapping.addAll(createAdditionalMappingsForTarget(target, mappingList)));
+    List<LinkedHashMap<String, Object>> truncatedMappingList = createTruncatedMappingList(mappingList);
+    targets.forEach(target ->  extendedMapping.addAll(createRelationsMappingForTarget(target, truncatedMappingList)));
     extendedMapping.addAll(regularMapping);
+    extendedMapping.addAll(new JsonArray(truncatedMappingList));
     return extendedMapping;
+  }
+
+  /**
+   * Creates a new mapping list from the original one
+   * where subfields "i" and numeric subfields are excluded
+   */
+  private List<LinkedHashMap<String, Object>> createTruncatedMappingList(
+    final List<LinkedHashMap<String, Object>> originalMappingList) {
+    List<LinkedHashMap<String, Object>> truncatedMappingList = new ArrayList<>();
+    originalMappingList.forEach(map -> {
+      LinkedHashMap<String, Object> truncatedMappingMap = new LinkedHashMap<>();
+      map.forEach((key, value) -> {
+        if (key.equals(TARGET)) {
+          truncatedMappingMap.put(TARGET, value + FIELDS_WITH_TRUNCATED_MAPPING_POSTFIX);
+          return;
+        }
+        if (key.equals(SUBFIELD)) {
+          List<String> subfieldList = (List) value;
+          List<String> truncatedSubfieldList = subfieldList.stream()
+            .filter(s -> !s.matches("[0-9i]"))
+            .toList();
+          truncatedMappingMap.put(SUBFIELD, truncatedSubfieldList);
+          return;
+        }
+        truncatedMappingMap.put(key, value);
+      });
+      truncatedMappingList.add(truncatedMappingMap);
+    });
+    return truncatedMappingList;
   }
 
   private List<String> retrieveTargetsFromControlSubfield(DataField dataField) {
     List<String> targets = new ArrayList<>();
+    if (dataField.getSubfield('w') == null) {
+      return targets;
+    }
     String subfieldData = dataField.getSubfield('w').getData();
     if (subfieldData.contains("g")) {
       targets.add("saftBroaderTerm");
@@ -1050,7 +1136,7 @@ public class Processor<T> {
    *     ]
    *   }
    */
-  private JsonArray createAdditionalMappingsForTarget(String target, List<LinkedHashMap<String, Object>> existingMappingList) {
+  private JsonArray createRelationsMappingForTarget(String target, List<LinkedHashMap<String, Object>> existingMappingList) {
     JsonArray additionalMappings = new JsonArray();
     existingMappingList.forEach(existingMap -> {
 
@@ -1075,7 +1161,7 @@ public class Processor<T> {
   }
 
   private static Object getHeadingType(Object headingField) {
-    String headingRType = headingField.toString().replace("saft", "");
+    String headingRType = headingField.toString().replace(SAFT_FIELDS_PREFIX, "");
     return Character.toLowerCase(headingRType.charAt(0)) + headingRType.substring(1);
   }
 }
